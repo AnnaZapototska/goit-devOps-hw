321c2258196adbd36abfaef6f72820356576951e
AnnaZapototska <zapototskaann@gmail.com>
2026-06-28 18:31:16 +0300

    Add Dockerized Django project with PostgreSQL and Nginx v2

--- app/config/settings.py
+++ app/config/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-san^r^yady4u&oa*yemef&j0(1hff$h!!mi_!q-@-g&x%&i8*#'
-
+SECRET_KEY = os.getenv("SECRET_KEY")
+    
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 
 # Application definition
